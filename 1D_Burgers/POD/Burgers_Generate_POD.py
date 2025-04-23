53de78fa198430f1ced403336463a2c9f4db579e
Liam-Magargal <lkm322@lehigh.edu>
2025-04-23 01:03:10 -0400

    Bug fixes

--- 1D_Burgers/POD/Burgers_Generate_POD.py
+++ 1D_Burgers/POD/Burgers_Generate_POD.py
@@ -3,11 +3,8 @@ import torch
 N = 256
 M = 10
 
-if torch.cuda.is_available():
-    device = torch.device('cuda')
-else:
-    device = torch.device('cpu')
+device = torch.device('cpu')
 
-w_hist_train = torch.load('w_train').to(device)
-U, sigma, V = torch.linalg.svd(w_hist_train)
-torch.save(U[:N,:M].cpu(), 'U_Burgers')
+w_hist_train = torch.load('../fullOrderModel/w_train').to(device)
+U, sigma, V = torch.linalg.svd(w_hist_train[:N,:])
+torch.save(U[:,:M].cpu(), 'U_Burgers')
