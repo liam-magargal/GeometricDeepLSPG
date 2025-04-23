8562a0cc44ee49f9e582ca10bc69a21b4ae36bb9
Liam-Magargal <lkm322@lehigh.edu>
2025-04-23 00:44:36 -0400

    Bug fixes

---
+++ 1D_Burgers/CNN/Burgers_comparison_515_0285.py
@@ -0,0 +1,20 @@
+import sys
+import torch
+
+if __name__=="__main__":
+    M = int(sys.argv[1])
+
+    w_truth = torch.load('../fullOrderModel/w_test_430_021')
+    N = w_truth.size()[0]
+    nt = w_truth.size()[1]
+    batchSize = 1
+
+    w_pred = torch.zeros((N,nt))
+    w_pred = torch.load('lat'+str(M)+'_430_021')
+
+    input_hist = w_truth[:,:]
+    pred_hist = w_pred[:,:]
+
+
+    full_rel_error = torch.linalg.vector_norm(torch.linalg.vector_norm(input_hist[:-30,:nt]-pred_hist[:,:nt], dim=0).flatten()) / torch.linalg.vector_norm(torch.linalg.vector_norm(input_hist[:-30,:nt], dim=0).flatten())
+    print(full_rel_error)
